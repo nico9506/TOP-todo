21c7ae8e0eb07b6e939c823fe9ef2de34302325e
Nicolas Lopez <niclopezt@gmail.com>
2023-11-27 15:33:28 +1100

    Set up webpack and add required plugins

---
+++ README.md
@@ -0,0 +1,1 @@
+This TODO app project takes part of the React pathway of The Odin Project.
---
+++ src/index.js
@@ -0,0 +1,6 @@
+import './style.css';
+
+const salute = document.createElement("h1");
+salute.textContent = "Hello from index.js";
+
+document.body.appendChild(salute);
