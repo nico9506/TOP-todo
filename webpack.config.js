76bfd2e303e84c3e6ab6a305423e3ac8f19da364
Nicolas Lopez <niclopezt@gmail.com>
2023-11-30 21:26:04 +1100

    Add animations and favicon

--- webpack.config.js
+++ webpack.config.js
@@ -7,6 +7,7 @@ module.exports = {
     plugins: [
         new HtmlWebpackPlugin({
             title: "TODO x Hacer!",
+            favicon: 'src/assets/favicon.ico',
         }),
     ],
     output: {
